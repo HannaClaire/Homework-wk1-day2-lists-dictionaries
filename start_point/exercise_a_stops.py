e676dd6924f9de11827723c381c76a0ea59ae0fe
Hanna <hannadurham3@gmail.com>
2023-04-04 17:16:33 +0100

    stops, complete

--- start_point/exercise_a_stops.py
+++ start_point/exercise_a_stops.py
@@ -1,21 +1,28 @@
 stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
 
-stops.append ("Edinburgh Waverley")
-stops.insert(0, "Glasgow Queen St") 
-stops.insert(4, "Polmont")
-print(stops)
+# stops.append ("Edinburgh Waverley")
+# stops.insert(0, "Glasgow Queen St") 
+# stops.insert(4, "Polmont")
 print(stops[5])
+stops.remove("Livingston")
+del (stops[1])
+print(len(stops))
+stops.sort()
+stops.reverse()
 
+for stop in range(len(stops)):
+    print(stops)
+    
 
-#1. Add "Edinburgh Waverley" to the end of the list DONE
-#2. Add "Glasgow Queen St" to the start of the list DONE
-#3. Add "Polmont" at the appropriate point (between "Falkirk High" and "Linlithgow") DONE
-#4. Print out the index position of "Linlithgow"
-#5. Remove "Livingston" from the list using its name
-#6. Delete "Cumbernauld" from the list by index
-#7. Print the number of stops there are in the list
-#8. Sort the list alphabetically
-#9. Reverse the positions of the stops in the list
-#10 Print out all the stops using a for loop
+#1. Add "Edinburgh Waverley" to the end of the list                                     DONE
+#2. Add "Glasgow Queen St" to the start of the list                                     DONE
+#3. Add "Polmont" at the appropriate point (between "Falkirk High" and "Linlithgow")    DONE
+#4. Print out the index position of "Linlithgow" DONE
+#5. Remove "Livingston" from the list using its name                                    DONE
+#6. Delete "Cumbernauld" from the list by index                                         DONE
+#7. Print the number of stops there are in the list                                     DONE
+#8. Sort the list alphabetically                                                        DONE
+#9. Reverse the positions of the stops in the list                                      DONE
+#10 Print out all the stops using a for loop                                            DONE
 
-
+print(stops)
